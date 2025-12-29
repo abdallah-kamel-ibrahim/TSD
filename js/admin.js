7632491cb9bc34f9273b4e3ac2e7baf8a72c4a6f
abdallah-kamel-ibrahim <84641504+abdallah-kamel-ibrahim@users.noreply.github.com>
2025-12-29 23:00:00 +0200

    Implement login check and product functions
    Added login check and product management functions.

--- js/admin.js
+++ js/admin.js
@@ -1,1 +1,23 @@
+if(localStorage.getItem("loggedIn") !== "true") {
+  alert("Please login first!");
+  window.location.href = "admin.html";
+}
 
+function loadProducts() {
+  fetch('products.json')
+    .then(res => res.json())
+    .then(data => {
+      // هنا هنعرض المنتجات في جدول مع Edit/Delete
+      console.log(data);
+    });
+}
+
+// Add Product
+function addProduct(title, price, image, description) {
+  let products = JSON.parse(localStorage.getItem('products') || '[]');
+  products.push({title, price, image, description});
+  localStorage.setItem('products', JSON.stringify(products));
+  alert("Product added!");
+}
+
+// Edit/Delete نفس الفكرة باستخدام index
